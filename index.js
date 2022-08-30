adf685b6b96cee0ad58fe0efbc8707134380fb30
Daniel Boley <danboley1@gmail.com>
2022-08-30 16:23:14 -0400

    commit tree 6a15af9700931fa417c64409a402a2f748349c44

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return newArray;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
